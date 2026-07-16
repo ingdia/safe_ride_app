d2c4f032975c8dc1893c8f7e7a4e98aef5b03564
elise-Iss15 <e.ishimwe3@alustudent.com>
2026-07-16 20:12:23 +0200

    feat: add models (User, School, Bus, Route)

---
+++ lib/features/admin/data/models/school_model.dart
@@ -0,0 +1,13 @@
+class SchoolModel {
+  final String schoolId;
+  final String name;
+  final String address;
+  final String adminId;
+
+  const SchoolModel({
+    required this.schoolId,
+    required this.name,
+    required this.address,
+    required this.adminId,
+  });
+}
