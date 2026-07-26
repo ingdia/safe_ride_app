a1dd4616c8fbd430408e33e0975fa5a23cff1d8f
mugisha-blaine <n.blaine@alustudent.com>
2026-07-26 16:48:50 +0200

    feat: finalize parent real-time trip streams

--- lib/features/parent/data/repositories/firestore_parent_repository.dart
+++ lib/features/parent/data/repositories/firestore_parent_repository.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../domain/entities/parent_dashboard_entity.dart';
 import '../../domain/entities/parent_notification_entity.dart';
 import '../../domain/entities/parent_trip_entity.dart';
-import '../../domain/repositories/parent_repository.dart';
+import '../../data/repositories/parent_repository.dart';
 import '../datasources/parent_firestore_paths.dart';
 import '../models/parent_trip_firestore_model.dart';
 import 'mock_parent_repository.dart';
