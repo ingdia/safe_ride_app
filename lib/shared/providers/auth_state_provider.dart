f26cfb9b4b45617d5f18875836a4d0856a5ba4dc
ingdia <d.ingabire2@alustudent.com>
2026-07-31 21:39:25 +0200

    Update shared authentication state provider

--- lib/shared/providers/auth_state_provider.dart
+++ lib/shared/providers/auth_state_provider.dart
@@ -1,1 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final firebaseAuthProvider = Provider<FirebaseAuth>((ref) {
+  return FirebaseAuth.instance;
+});
+
+final authStateProvider = StreamProvider<User?>(
+  (ref) => ref.watch(firebaseAuthProvider).authStateChanges(),
+);
 
